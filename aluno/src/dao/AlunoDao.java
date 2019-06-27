9ab658b45046e10c2615002ce64335a61c434666
Administrador <Administrador@10.10.4.18>
2019-06-27 16:39:34 -0300

    dao

---
+++ aluno/src/dao/AlunoDao.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Aluno;
+
+/**
+ *
+ * @author Administrador
+ */
+public class AlunoDao {
+    public static boolean inserir(Aluno objeto) {
+        String sql = "INSERT INTO aluno (nome, endereco) VALUES (?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome());
+            ps.setString(2, objeto.getEndereco());
+            
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+     public static void main(String[] args) {
+        Aluno objeto = new Aluno();
+        objeto.setNome("Joao");
+        objeto.setEndereco("Ibiruba");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+    
+}
